cc43ea63d9a8475d783c9e6e3f6ce5117b86bb18
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2025-10-13 21:21:29 -0700

    update version to reflect forge

--- HeadVoiceSelector.cs
+++ HeadVoiceSelector.cs
@@ -6,7 +6,7 @@ using System.IO;
 
 namespace HeadVoiceSelector
 {
-    [BepInPlugin("com.HeadVoiceSelector.Core", "HeadVoiceSelector Core", "1.0.4")]
+    [BepInPlugin("com.HeadVoiceSelector.Core", "HeadVoiceSelector Core", "1.0.6")]
 
     internal class HeadVoiceSelector : BaseUnityPlugin
     {
